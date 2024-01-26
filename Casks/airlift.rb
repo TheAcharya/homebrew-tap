3ae80d53c2ca1bc93413affd17942a00e036d633
Arjun <arjunprakash027@gmail.com>
2024-01-26 15:41:06 +0530

    feature:added Casks folder and airlift.rb file

---
+++ Casks/airlift.rb
@@ -0,0 +1,16 @@
+cask "airlift" do
+    version "1.0.6"
+    sha256 :no_check
+    
+    airlift_version = "1.0.6"
+    url "https://github.com/TheAcharya/Airlift/releases/download/v#{airlift_version}/Airlift_#{airlift_version}.pkg"
+    name "airlift"
+    desc "A command-line tool to upload csv/json to airtable"
+    homepage "https://github.com/TheAcharya/Airlift"
+  
+    pkg "Airlift_#{airlift_version}.pkg"   
+  
+    uninstall pkgutil: [
+      "com.bps.Airlift_#{airlift_version}.pkg"
+    ]
+  end
