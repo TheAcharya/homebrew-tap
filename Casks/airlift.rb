aeb8d9f23e877c17782b5e2985346edf56491537
Arjun P <72484657+arjunprakash027@users.noreply.github.com>
2024-01-26 18:02:02 +0530

    Update airlift.rb
    test changes

--- Casks/airlift.rb
+++ Casks/airlift.rb
@@ -2,7 +2,7 @@ cask "airlift" do
     version "1.0.6"
     sha256 :04c6cdd0f0ba7879a65833ed44edcf5a4dd8383848c99f9e36ea243dcec7e7d2
       
-    airlift_version = "1.0.6"
+    airlift_version = ""
     url "https://github.com/TheAcharya/Airlift/releases/download/v#{airlift_version}/Airlift_#{airlift_version}.pkg"
     name "Airlift"
     desc "Upload & Merge Data with Attachments to Airtable"
