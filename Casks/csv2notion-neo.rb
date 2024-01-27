6968c27c9dd57660051ff1c80229a7757bbd3c12
Vigneswaran Rajkumar <118706051+IAmVigneswaran@users.noreply.github.com>
2024-01-27 15:18:33 +0800

    Update csv2notion-neo.rb

--- Casks/csv2notion-neo.rb
+++ Casks/csv2notion-neo.rb
@@ -2,7 +2,7 @@ cask "csv2notion-neo" do
   version "1.2.2"
   sha256 "caad9ccd1d9eaab660dc53deeaba0b9ed6570e9c7e8f06df0204a15d178fc823"
   url "https://github.com/TheAcharya/csv2notion-neo/releases/download/v1.2.2/CSV2Notion-Neo_1.2.2.pkg"
-  name "Csv2notionNeo"
+  name "CSV2Notion Neo"
   desc "Upload & Merge Data with Attachments to Notion"
   homepage "https://github.com/TheAcharya/csv2notion-neo"
 
