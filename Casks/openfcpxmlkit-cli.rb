add8e85447254cf174968c32a9e482e299aa07b4
Vigneswaran Rajkumar <118706051+IAmVigneswaran@users.noreply.github.com>
2026-07-02 06:33:04 +0800

    Create openfcpxmlkit-cli.rb

---
+++ Casks/openfcpxmlkit-cli.rb
@@ -0,0 +1,12 @@
+cask "openfcpxmlkit-cli" do
+  version "3.0.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/TheAcharya/OpenFCPXMLKit/releases/download/#{version}/OpenFCPXMLKit-CLI-#{version}.pkg"
+  name "OpenFCPXMLKit"
+  desc "Swift framework for Final Cut Pro FCPXML processing (CLI)"
+  homepage "https://github.com/TheAcharya/OpenFCPXMLKit"
+
+  pkg "OpenFCPXMLKit-CLI-#{version}.pkg"
+
+  uninstall pkgutil: "co.theacharya.OpenFCPXMLKit"
+end
