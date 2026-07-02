07b4425a953391db2ac5b00c7eb98ade90a81d7c
Vigneswaran Rajkumar <118706051+IAmVigneswaran@users.noreply.github.com>
2026-07-02 09:09:11 +0800

    Updated OpenFCPXMLKit-CLI formula to version 3.0.0

--- Casks/openfcpxmlkit-cli.rb
+++ Casks/openfcpxmlkit-cli.rb
@@ -1,6 +1,6 @@
 cask "openfcpxmlkit-cli" do
   version "3.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "ed6cf07e782dbd705eab2764df51e82890c6f78d5fa223c327d3f5f0e154e1f6"
   url "https://github.com/TheAcharya/OpenFCPXMLKit/releases/download/#{version}/OpenFCPXMLKit-CLI-#{version}.pkg"
   name "OpenFCPXMLKit"
   desc "Swift framework for Final Cut Pro FCPXML processing (CLI)"
